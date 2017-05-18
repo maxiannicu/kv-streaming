d9f5ac8e5f74da0a6429f128cbd9bea95fa96a14
Schidu Luca <schiduluca@gmail.com>
2017-05-18 17:14:11 +0300

    Added Welcome screen UI, JavaFX with FXML and stylesheets

--- src/main/java/sample/Main.java
+++ src/main/java/sample/Main.java
@@ -9,7 +9,7 @@ import javafx.stage.Stage;
 public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/welcomeScreen.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 800, 600));
         primaryStage.show();
